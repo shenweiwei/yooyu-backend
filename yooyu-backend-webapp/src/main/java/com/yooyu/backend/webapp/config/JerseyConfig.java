9d980bfc48522bc6f426a5310a0ef58b923e29b0
shenweiwei <shenweiwei9@163.com>
2018-01-10 15:47:53 +0800

    update

--- yooyu-backend-webapp/src/main/java/com/yooyu/backend/webapp/config/JerseyConfig.java
+++ yooyu-backend-webapp/src/main/java/com/yooyu/backend/webapp/config/JerseyConfig.java
@@ -4,6 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Path;
 import javax.ws.rs.ext.Provider;
 
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.reflections.Reflections;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ public class JerseyConfig extends ResourceConfig {
 			Reflections reflections = new Reflections(pack);
 			registerClasses(reflections.getTypesAnnotatedWith(Path.class));
 			registerClasses(reflections.getTypesAnnotatedWith(Provider.class));
+			registerClasses(MultiPartFeature.class); 
 		}
 	}
 }
