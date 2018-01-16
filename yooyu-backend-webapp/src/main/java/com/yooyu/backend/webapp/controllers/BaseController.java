fb2ee6f0d769cebfd46c6f929d8d98d0de1c2e95
weiweiShen <shenweiwei9@163.com>
2018-01-16 13:59:49 +0800

    update

--- yooyu-backend-webapp/src/main/java/com/yooyu/backend/webapp/controllers/BaseController.java
+++ yooyu-backend-webapp/src/main/java/com/yooyu/backend/webapp/controllers/BaseController.java
@@ -1,15 +1,5 @@
 package com.yooyu.backend.webapp.controllers;
 
-import javax.annotation.Resource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-
-import com.yooyu.backend.common.utils.RedisUtil;
-
 public abstract class BaseController {
 	
 	
